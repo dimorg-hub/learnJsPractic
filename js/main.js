4452f3a0099628bba85ddd143214bf9055b33bd1
Дмитрий Попов <dimorg30@gmail.com>
2022-12-20 06:50:00 +0300

    2 practic lessons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
     count: numbersOfFilms,
@@ -8,12 +8,25 @@ let personalMovieDB = {
     privat: false
 };
 
-let a = prompt('Один из просмотренных фильмов?', '');
-let b = prompt('На сколько вы его оцените?', '');
-let c = prompt('Один из просмотренных фильмов?', '');
-let d = prompt('На сколько вы его оцените?', '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из просмотренных фильмов?', '');
+    let b = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы класический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка!!!');
+}
 
 console.log(personalMovieDB);
